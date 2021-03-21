5b179c388bbfd7b488aea68fe580cfa4e7352e46
Vedat Bozkurt <info@wedat.org>
2021-03-21 12:41:33 +0300

    babel config updated

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
+/*
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2021-03-21 12:41:16
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2021-03-21 12:41:16
+ */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { "legacy": true}],
+],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
