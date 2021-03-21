b17301531da84917a7b4ffd524dddd0dbcb751f6
Vedat Bozkurt <info@wedat.org>
2021-03-21 13:27:21 +0300

    login screen

--- screens/Auth/LoginScreen.js
+++ screens/Auth/LoginScreen.js
@@ -3,5 +3,33 @@
  * @Email: info@wedat.org
  * @Date: 2021-03-21 13:26:38
  * @LastEditors: @vedatbozkurt
- * @LastEditTime: 2021-03-21 13:26:39
+ * @LastEditTime: 2021-03-21 13:27:07
  */
+import React, { Component } from "react";
+import { Button, Text, View } from "react-native";
+
+class LoginScreen extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+        payments: '',
+    };
+}
+
+componentDidMount() {
+    // this.getPayments();
+}
+
+  render(props) {
+    return (
+      <View>
+        <Text>
+          I am , and I am LoginScreen!
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default LoginScreen;
