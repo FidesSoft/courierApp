1a09660d4d41cae105d7b4f4b501eeb31be33d83
Vedat Bozkurt <info@wedat.org>
2021-03-22 20:02:40 +0300

    iban index

---
+++ routes/auth/IbanStack.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: @vedatbozkurt
+ * @Email: info@wedat.org
+ * @Date: 2021-03-22 20:01:08
+ * @LastEditors: @vedatbozkurt
+ * @LastEditTime: 2021-03-22 20:01:40
+ */
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+// auth
+import IbanIndex from '../../screens/Iban/IbanIndex';
+import DefaultAppbar from '../../components/Appbar/DefaultAppbar';
+
+const Stack = createStackNavigator();
+
+export default function IbanStack() {
+    return (
+        <Stack.Navigator
+        screenOptions={{
+            header: (props) => <DefaultAppbar {...props} />
+        }} >
+            <Stack.Screen name="IbanIndex" component={IbanIndex}
+                initialParams={{ screenTitle: 'Iban Listesi', screenDescription: 'Hızlı ödeme için ibanlar ekleyin.' }}
+            />
+            <Stack.Screen name="IbanCreate" component={IbanCreate}
+                initialParams={{ screenTitle: 'Yeni İban Ekle', screenDescription: '26 haneli iban numaranizi oluşturun.' }}
+            />
+            <Stack.Screen name="IbanEdit" component={IbanEdit}
+                initialParams={{ screenTitle: 'İban Düzenle', screenDescription: 'İban numaranızı düzenleyin.' }}
+            />
+        </Stack.Navigator>
+    );
+}
